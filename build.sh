701d215063d2ae22747c678f70f0bf5ac0f89650
Xun Lian <xunlian@FFA-IN-CIL.local>
2018-08-06 19:50:25 +0800

    she脚本

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 dir=$(ls -l ./src |awk '/^d/ {print $NF}')
-cur_date="`date +%Y%m%d`" 
+cur_date="`date +%Y%m%d%H%M%S`" 
 for i in $dir
 do
 npx cross-env ENV_file=${i} webpack --config ./build/webpack.pro.js 
 echo "打包的文件夹是: $i"
 done
-cd dist
-tar -zcvf ../sparrow_${cur_date}.tar.gz ./*
-echo "打包完毕""
+mv dist/ sparrow_${cur_date}
+tar -zcvf ./sparrow_${cur_date}.tar.gz ./sparrow_${cur_date}/*
+rm -r sparrow_${cur_date}
+echo "打包完毕"
